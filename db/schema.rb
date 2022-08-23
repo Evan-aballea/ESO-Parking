8feea6b24bde2db6475d842a7ae2aa6abb83c982
Olivier Barthelemy <olivier.barthelemy1993@gmail.com>
2022-08-23 15:23:37 +0200

    seed_with_img+add_to_pkg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_100134) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_131741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_100134) do
     t.time "availability_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_parkings_on_user_id"
   end
 

04c8b05263ccd2b6547e90dbc266892044128c18
Olivier Barthelemy <olivier.barthelemy1993@gmail.com>
2022-08-23 12:09:05 +0200

    cloudinary + active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_many :parkings
+  has_one_attached :photo
 
   validates :first_name, :last_name, :email, :phone_number, presence: true
   validates :email, uniqueness: true

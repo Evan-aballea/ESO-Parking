8a4b424d48b7fbc874e8148340b8f0d79cc6e1d8
Olivier Barthelemy <olivier.barthelemy1993@gmail.com>
2022-08-24 11:09:10 +0200

    seed_+_switch_image_+_showv3

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,26 @@
 class BookingsController < ApplicationController
-
+  before_action :set_parking, only: %i[create]
   def index
 
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.parking = @parking
+    @booking.save
+    # redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
+  end
+
+  private
+
+  def set_parking
+    @parking = Parking.find(params[:parking_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:arrival)
   end
 end
